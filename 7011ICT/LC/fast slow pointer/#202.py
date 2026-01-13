93bf68d0686ad6af41a560e7daf400e08adb6d7b
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2026-01-14 09:55:10 +1000

    fast and slow pointer

---
+++ 7011ICT/LC/fast slow pointer/#202.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isHappy(self, n):
+        def get_next(num):
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit * digit
+                num //= 10
+            return total
+
+        slow = n
+        fast = get_next(n)
+
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+
+        return fast == 1
